e9e2260ee857cea064aeefb506c8ae39d5e383cc
Joakim Larsson <joakim.larsson.96@gmail.com>
2025-01-13 23:01:49 +0100

    Starting work on new globe rendering and tile handling, such that the render elements and gameplay elements are more clearly separated

--- Engine7/Engine/Sandbox/Logic/World/Tiles2/Tile.cs
+++ Engine7/Engine/Sandbox/Logic/World/Tiles2/Tile.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Engine.Standard.Render.Meshing;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,11 +8,11 @@ using System.Xml;
 
 namespace Sandbox.Logic.World.Tiles2;
 public interface INode {
-	TileRenderModel RenderModel { get; }
+	//TileRenderModel RenderModel { get; }
 }
 
 public abstract class NodeBase : INode {
-	public TileRenderModel RenderModel { get; }
+	//public TileRenderModel RenderModel { get; }
 }
 
 public sealed class Province : NodeBase {
@@ -34,6 +35,47 @@ public sealed class Cluster : NodeBase {
 
 }
 
-public sealed class TileRenderModel {
+public sealed class GlobeTile {
+
+	public Globe Globe { get; }
+
+	public int IndexA { get; }
+	public int IndexB { get; }
+	public int IndexC { get; }
+
+	public Vector3<float> VectorA => Globe.Vertices[ IndexA ];
+	public Vector3<float> VectorB => Globe.Vertices[ IndexB ];
+	public Vector3<float> VectorC => Globe.Vertices[ IndexC ];
+
+	public GlobeTile(Globe globe, int indexA, int indexB, int indexC ) {
+		this.Globe = globe;
+		this.IndexA = indexA;
+		this.IndexB = indexB;
+		this.IndexC = indexC;
+	}
+}
+
+public sealed class Globe {
+
+	//TODO maybe a light version of composite pattern, where "Tiles" have components, such that we don't need many versions of the "same" class just to have different tile types.
+	public uint Layers { get; }
+	public int RootLayer { get; }
+
+	public IReadOnlyList<Vector3<float>> Vertices { get; }
+	private List<GlobeTile> _rootTiles;
+
+	public Globe(uint layers = 8, int rootLayer = 4) {
+		this.Layers = layers;
+		this.RootLayer = rootLayer;
+
+		Icosphere icosphere = new( layers, rootLayer );
+		this.Vertices = icosphere.Vertices.ToList().AsReadOnly();
+
+		GenerateMosaic(icosphere);
+	}
+
+	private void GenerateMosaic( Icosphere icosphere ) {
+
+	}
 
 }
