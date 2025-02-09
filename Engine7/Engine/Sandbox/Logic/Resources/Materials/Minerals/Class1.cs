b724f7e1b99e1511e300df682b703bfb741860c3
Joakim Larsson <joakim.larsson.96@gmail.com>
2025-02-09 23:27:19 +0100

    Technology!!!

---
+++ Engine7/Engine/Sandbox/Logic/Resources/Materials/Minerals/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.Logic.Resources.Materials.Minerals;
+internal class Class1 {
+}
+//https://en.wikipedia.org/wiki/List_of_minerals
