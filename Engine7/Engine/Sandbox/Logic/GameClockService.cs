b1f1801c935363513b4e3417bd02fe0e26f1bc2b
Joakim Larsson <joakim.larsson.96@gmail.com>
2025-02-17 22:29:01 +0100

    Tech tech society time tracking

---
+++ Engine7/Engine/Sandbox/Logic/GameClockService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.Logic;
+public sealed class GameClockService : IUpdateable {
+	public void Update( double time, double deltaTime ) {
+
+	}
+}
+
+public sealed class GameClock {
+
+	private double _daysSinceStart;
+
+	public GameClock( ulong daysSinceStart = 0 ) {
+		_daysSinceStart = daysSinceStart;
+	}
+
+	//TODO: need a way to slowly ramp down the days passing each tick as we near the end of the game
+
+	public double YearsSinceStart => _daysSinceStart / 365.24225;
+
+}
